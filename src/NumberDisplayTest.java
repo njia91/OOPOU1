59af8cf424eeff3ed7a8909fd7e0f0d305256422
njia91 <m-andersson@outlook.com>
2016-04-02 17:10:26 +0200

    test

--- src/NumberDisplayTest.java
+++ src/NumberDisplayTest.java
@@ -3,13 +3,18 @@ import org.junit.Test;
 import static junit.framework.TestCase.assertEquals;
 
 /**
- * Created by dv15man on 2016-03-30.
+ * Created by dv15man
  */
 public class NumberDisplayTest {
 
     @Test
     public void shouldCreateNumberDisplay() throws Exception{
         new NumberDisplay(0,1);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void shouldNotCreateNumberDisplay() throws Exception{
+        new NumberDisplay(1,0);
     }
 
     @Test
@@ -58,16 +63,36 @@ public class NumberDisplayTest {
     }
 
     @Test(expected = IllegalStateException.class)
-    public void shouldNotGetCorrectDigit() throws Exception{
-        NumberDisplay display = new NumberDisplay(0,12);
+    public void shouldNotGetCorrectDigit() throws Exception {
+        NumberDisplay display = new NumberDisplay(0, 12);
 
         display.setValue(5);
 
-       if (display.getValue() != 7){
-           throw new IllegalStateException("Returns wrong value");
-       }
+        if (display.getValue() != 7) {
+            throw new IllegalStateException("Returns wrong value");
+        }
+    }
 
+    @Test
+    public void shouldGetDisplayValue() throws Exception{
+        NumberDisplay display = new NumberDisplay(0,12);
 
+        display.setValue(5);
+        System.out.println(display.getDisplayValue());
+        if (!(display.getDisplayValue()).equals("05")){
+            throw new IllegalStateException("Returns wrong String");
+        }
+    }
+
+    @Test
+    public void shouldGetDubbleZero() throws Exception{
+        NumberDisplay display = new NumberDisplay(0,12);
+
+        display.setValue(0);
+        System.out.println(display.getDisplayValue());
+        if (!(display.getDisplayValue()).equals("00")){
+            throw new IllegalStateException("Returns wrong String");
+        }
     }
 
 
